85b0deb59df4c3bdbb009ddb54494c5d4cf01405
ultasun <ultasun@icloud.com>
2022-11-06 18:16:43 -0500

    completed section up to 'reflecting database objects'. the comment containing the ellipsis on line 173 is where to place the code from the subsection titled 'combinding core table declarations with orm declarative'.

--- tutorial.py
+++ tutorial.py
@@ -97,3 +97,80 @@ with Session(engine) as session:
         )
     session.commit()
 
+# -----------------------------------------------------------------------------
+# working with database metadata
+
+# setting up metadata with table objects
+from sqlalchemy import MetaData
+metadata_obj = MetaData()
+
+from sqlalchemy import Table, Column, Integer, String
+user_table = Table(
+    "user_account",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("name", String(30)),
+    Column("fullname", String),
+)
+
+# declaring simple constraints
+from sqlalchemy import ForeignKey
+address_table = Table(
+    "address",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", ForeignKey("user_account.id"), nullable=False),
+    Column("email_address", String, nullable=False),
+)
+
+# emitting DDL to the database
+metadata_obj.create_all(engine)
+
+# defining table metadata with the orm
+
+# setting up the Registry
+from sqlalchemy.orm import registry
+mapper_registry = registry()
+Base = mapper_registry.generate_base()
+
+# another alternative
+#from sqlalchemy.orm import declarative_base
+#Base = declarative_base()
+
+# declaring mapped classes
+from sqlalchemy.orm import relationship
+class User(Base):
+    __tablename__ = "user_account"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    fullname = Column(String)
+
+    addresses = relationship("Address", back_populates="user")
+
+    def __repr__(self):
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+class Address(Base):
+    __tablename__ = "address"
+
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("user_account.id"))
+
+    user = relationship("User", back_populates="addresses")
+
+    def __repr__(self):
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+
+# other mapped class details
+sandy = User(name="sandy", fullname="Sandy Cheeks")
+
+# emitting ddl to the database
+mapper_registry.metadata.create_all(engine)
+Base.metadata.create_all(engine)
+
+# ...
+
+# table reflection
+some_table = Table("some_table", metadata_obj, autoload_with=engine)
