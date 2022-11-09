e69120a735d009492a7ae08a545674b87e689b5f
Ulta Sun <ultasun@icloud.com>
2022-11-09 16:03:40 -0500

    'correlated updates'

--- tutorial.py
+++ tutorial.py
@@ -774,3 +774,13 @@ with engine.begin() as conn:
         ],
     )
 
+# correlated updates
+scalar_subq = (
+    select(address_table.c.email_address)
+    .where(address_table.c.user_id == user_table.c.id)
+    .order_by(address_table.c.id)
+    .limit(1)
+    .scalar_subquery()
+)
+update_stmt = update(user_table).values(fullname=scalar_subq)
+print(update_stmt)
